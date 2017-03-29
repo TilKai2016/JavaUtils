aa1c274f7007d92264beac8ff3f61f923af24abc
chilonghai <chilonghai@thsolar.com>
2017-03-29 18:20:30 +0800

    时间工具添加时间字符串与Calendar和Date的转换

--- common-utils/src/main/java/utils/DateUtils.java
+++ common-utils/src/main/java/utils/DateUtils.java
@@ -1,6 +1,9 @@
 package utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author tilkai
@@ -194,4 +197,34 @@ public class DateUtils {
 //        }
 //
 //    }
+
+    /**
+     * 时间字符串转Calendar
+     * @param dateStr 时间字符串
+     * @param format 格式化
+     * @return Calendar
+     * @throws ParseException
+     */
+    public static Calendar dateStr2Calendar(String dateStr, String format) throws ParseException {
+
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(dateStr2Date(dateStr, format));
+
+        return calendar;
+    }
+
+    /**
+     * 时间字符串转Date
+     * @param dateStr
+     * @param format
+     * @return
+     * @throws ParseException
+     */
+    public static Date dateStr2Date(String dateStr, String format) throws ParseException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+
+        return dateFormat.parse(dateStr);
+    }
 }
